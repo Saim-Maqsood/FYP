c3ce2054a9252a54f08a512161a5908afdbe8070
Saim-Maqsood <97038010+Saim-Maqsood@users.noreply.github.com>
2025-02-17 23:48:01 +0500

    update code

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -6,11 +6,12 @@ const Navbar = () => {
   return (
     <div className="w-full text-white">
       <Image src={"/Logo.jpg"} width={200} height={300} alt="Logo" className="m-auto w-5 h-5 sm:w-10 sm:h-10 md:w-16 md:h-16 lg:w-20 lg:h-20 xl:w-24 xl:h-24 2xl:w-32 2xl:h-32" />
-      <div className="flex justify-center">
-        <Link href="/" className="pr-2">Home</Link>
-        <Link href="/store" className="pr-2">Store</Link>
-        <Link href="/about" className="pr-2">About Us</Link>
-        <Link href="/contact" className="pr-2">Contact Us</Link>
+      <div className="flex justify-center flex-wrap">
+        <Link href="/" className="p-2 text-xs sm:text-sm md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">Home</Link>
+        <Link href="/store" className="p-2 text-xs sm:text-sm md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">Store</Link>
+        <Link href="/about" className="p-2 text-xs sm:text-sm md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">About Us</Link>
+        <Link href="/contact" className="p-2 text-xs sm:text-sm md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">Contact Us</Link>
+        <Link href="/" className="p-2 text-xs sm:text-sm md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">FAQs</Link>
       </div>
     </div>
   )
