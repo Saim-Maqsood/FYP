7226b8fb73327adf03e8021766f579e454e480c8
Saim-Maqsood <97038010+Saim-Maqsood@users.noreply.github.com>
2025-02-22 12:27:21 +0500

    update code

--- app/components/SignIn.jsx
+++
@@ -1,6 +0,0 @@
-"use client"
-import { signIn } from "next-auth/react"
- 
-export default function SignIn() {
-  return <button onClick={() => signIn()} className="bg-white px-2 py-2 rounded-md">Sign In</button>
-}
